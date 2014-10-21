79378b5f492b0f00da83cafdc3334270ebbf5721
Anastasia Stolyarova <taisniqm@gmail.com>
2014-10-21 21:31:23 +0000

    lattice detection (out: 'hon', 'sq' or None)

---
+++ val_lattice.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import json, sys
+
+
+def val_lattice(file):
+    lines = file.readlines()
+    stringl = ""
+    for line in lines:
+        stringl += line
+    obj = json.loads(stringl)
+    numBases = len(obj['vstrands'][0]['scaf'])
+    if numBases % 32 == 0:
+        return 'sq'
+    elif numBases % 21 == 0:
+        return 'hon'
+    else:
+        return None
+
+with open(sys.argv[1], 'r') as file:
+    print val_lattice(file)
