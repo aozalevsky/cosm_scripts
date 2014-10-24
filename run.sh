d51b756654885030ecbef817055ba90ce1f3868d
Arthur Zalevsky <aozalevsky@gmail.com>
2014-10-25 01:56:28 +0400

    handle error of python scripts

--- run.sh
+++ run.sh
@@ -27,8 +27,22 @@ export TEMPLATE_PATH=${BASE}/static/templates
 
 
 function error_exit {
-  echo $1
+  echo "<ERROR: ${1}>"
   exit 1
+}
+
+
+function python_wrapper {
+  error=$(${1} 2>&1 | grep '^Ex' | sed 's/Exception: //')
+  if [[ -n $error ]]
+    then 
+      echo "<ERROR: ${error}>"
+      exit 1
+  fi
+}
+
+function stage {
+  echo "<STAGE: ${1}>"
 }
 
 
@@ -64,13 +78,13 @@ trap - EXIT
 
 #set -e
 
-echo '<STAGE: 1>'
+stage 1
 
 job=${1%.json}
 
-if [ $# != 4 ]
+if [[ $# != 4 ]]
 then
-   if [ $# != 5 ]
+   if [[ $# != 5 ]]
     then
         exit 1
     else
@@ -78,24 +92,24 @@ then
     #cp $4 $seq
     #oligs=$(basename $5)
     #cp $5 $oligs
-    json2cosm.py -i ${job}.json -o ${job}.pdb -r ${job}_r -t ${job}_t -l $2 --seq $4 --oligs $5 || error_exit "<ERROR: Unable to convert structure>"
+    python_wrapper "json2cosm.py -i ${job}.json -o ${job}.pdb -r ${job}_r -t ${job}_t -l ${2} --seq ${4} --oligs ${5}"
     fi
 else
-json2cosm.py -i ${job}.json -o ${job}.pdb -r ${job}_r -t ${job}_t -l $2 || error_exit "<ERROR: Unable to convert structure>"
+python_wrapper "json2cosm.py -i ${job}.json -o ${job}.pdb -r ${job}_r -t ${job}_t -l ${2}"
 fi
 
 
 ### Step 2
 
-echo '<STAGE: 2>'
+stage 2
 
-if [ "${2::1}" = "h" ]
+if [[ "${2::1}" = "h" ]]
 then 
 ccg="cosm002"
 else
 ccg="cosm002sq"
 fi
-if [ "$3" == "0" ]
+if [[ "$3" == "0" ]]
 then
 nt=""
 else
@@ -103,36 +117,36 @@ nt=" -nt $3"
 fi
 
 
-pdb2gmx -f ${job}.pdb -o beg.gro -merge all -ff ${ccg} -water none || error_exit  "<ERROR: Unable to create topology file>"
+pdb2gmx -f ${job}.pdb -o beg.gro -merge all -ff ${ccg} -water none || error_exit  "Unable to create topology file"
 awk -v name=${job} '/; Include Position restraint file/{print "#include \"" name "_r\""}1' topol.top > topol_tmp 
 mv topol_tmp topol.top 
 
 
-echo '<STAGE: 3>'
-grompp -f ${GMXLIB}/${ccg}.ff/minl.mdp -c beg -p topol -o em || error_exit "<ERROR: Unable to perform energy minimization (run 1)>"
-mdrun -deffnm em -v || error_exit "<ERROR: Unable to perform energy minimization (run 1)>"
+stage 3
+grompp -f ${GMXLIB}/${ccg}.ff/minl.mdp -c beg -p topol -o em || error_exit "Unable to perform energy minimization (run 1)"
+mdrun -deffnm em -v || error_exit "Unable to perform energy minimization (run 1)"
 
 
-echo '<STAGE: 4>'
-grompp -f ${GMXLIB}/${ccg}.ff/min-implicit.mdp -c em -p topol -o em2 || error_exit "<ERROR: Unable to perform energy minimization (run 2)>"
-mdrun -deffnm em2 -pd$nt -v || error_exit "<ERROR: Unable to perform energy minimization (run 2)>"
+stage 4
+grompp -f ${GMXLIB}/${ccg}.ff/min-implicit.mdp -c em -p topol -o em2 || error_exit "Unable to perform energy minimization (run 2)"
+mdrun -deffnm em2 -pd$nt -v || error_exit "Unable to perform energy minimization (run 2)"
 
 
-echo '<STAGE: 5>'
-mkmd 0 || error_exit "<ERROR: Unable to perform MD>"
+stage 5
+mkmd 0 || error_exit "Unable to perform MD"
 
-echo '<STAGE: 6>'
-echo 1 | trjconv -f md.gro -s md -o ${job}_end.pdb -conect | error_exit "<ERROR: Unable to prepare output files>"
-echo 1 | trjconv -f md -s md -o ${job}_md.pdb -conect -skip 500 | error_exit "<ERROR: Unable to prepare output files>"
+stage 6
+echo 1 | trjconv -f md.gro -s md -o ${job}_end.pdb -conect || error_exit "Unable to prepare output files"
+echo 1 | trjconv -f md -s md -o ${job}_md.pdb -conect -skip 500 || error_exit "Unable to prepare output files"
 
 
-echo '<STAGE: 7>'
-if [ $# == 4 ]
+stage 7
+if [[ $# == 4 ]]
 then
-    cosm2full.py -i ${job}_end.pdb -t ${job}_t -l $2 -p $4 -o ${job}_end_full.pdb || echo "<ERROR: Unable to convert full atom structure>"
+    python_wrapper "cosm2full.py -i ${job}_end.pdb -t ${job}_t -l $2 -p $4 -o ${job}_end_full.pdb"
 else
-    cosm2full.py -i ${job}_end.pdb -t ${job}_t -l $2 -s $5 -o ${job}_end_full.pdb || echo "<ERROR: Unable to convert full atom structure>"
+    python_wrapper "cosm2full.py -i ${job}_end.pdb -t ${job}_t -l $2 -s $5 -o ${job}_end_full.pdb"
 fi
-appendcnct.py ${job} || echo "<ERROR: Unable to convert full atom structure>"
+python_wrapper "appendcnct.py ${job}"
 
 exit $?
