9dcfbc93991797a51406a9fd60c588ccc4340888
Arthur Zalevsky <aozalevsky@gmail.com>
2017-10-01 22:54:21 +0300

    dihedral restraints

--- cosm_restr.py
+++ cosm_restr.py
@@ -74,6 +74,31 @@ def remove_period(a, mult=2):
     return(a - per * np.floor_divide(a, per))
 
 
+def divide_ds(DS, coords):
+    DS_ = list()
+
+    for ds in DS:
+        ds_ = list()
+        x = coords[ds[0]][0]
+        y = coords[ds[0]][1]
+
+        for p in ds:
+            x_ = coords[p][0]
+            y_ = coords[p][1]
+            if (x_ == x) and (y_ == y):
+                pass
+            else:
+               DS_.append(ds_)
+               ds_ = list()
+               x = x_
+               y = y_
+            ds_.append(p)
+
+    return DS_
+                 
+
+        
+
 # In[74]:
 
 def get_cg_struct(path):
@@ -137,6 +162,8 @@ class Topology(object):
     fname = None
     e3 = None
     e5 = None
+
+    torsions = None
 
     def __init__(self):
         pass
@@ -275,6 +302,8 @@ while N < len(struct):
 if len(ds) > 0:
     DS.append(ds)
 
+DS = divide_ds(DS, coords)
+
 # In[120]:
 # http://stackoverflow.com/questions/952914/making-a-flat-list-out-of-list-of-lists-in-python
 junctions = np.array(sorted([item for sublist in scross for item in sublist]))
@@ -369,16 +398,33 @@ cgtop.lattice = sorted(set(lattice))
 # Torsions
 
 torsions = list()
-for ds_i in DS:
-    for ds_j in DS:
+
+
+for ds_i_ in range(len(DS)):
+    ds_i = DS[ds_i_]
+    
+    if len(ds_i) == 1:
+        continue
+
+    for ds_j_ in range(ds_i_):
+        ds_j = DS[ds_j_]
+        if len(ds_j) == 1:
+            continue
+
         t_s, t_e = None, None
 
         for s in scross:
-            i_, j_ = s
 
-            if i_ in ds_i and j_ in ds_j:
-                pass
+            if (s[0] in ds_i and s[1] in ds_j):
+            	i_, j_ = s
+
+            elif (s[1] in ds_i and s[0] in ds_j):
+            	j_, i_ = s
+		
             else:
+                continue
+
+            if rlabels[i_] != 'H' or rlabels[j_] != 'H':
                 continue
 
             if not t_s or i_ < t_s[0]:
@@ -386,12 +432,14 @@ for ds_i in DS:
             elif not t_e or j_ > t_e[0]:
                 t_e = s
 
-        torsions.append((
-            t_s[1], t_s[0],
-            t_e[1], t_e[0],
-        ))
 
-print(torsions)
+	if t_s and t_e:
+	        torsions.append((
+        	    t_s[1], t_s[0],
+	            t_e[0], t_e[1],
+        	))
+
+cgtop.torsions = torsions
 
 # In[129]:
 
@@ -413,8 +461,16 @@ def write_fullatom_pdb(fname, top):
     RF = '%d %d 1 %d 1 '
 
     with open(fname, 'w') as f:
+        f.write('\n[ dihedrals ]\n')
+        f.write('; torsion angles\n')
+        
+        for t in top.torsions:
+            t = map(lambda x: int(x) + 1, t)
+            f.write(' '.join(map(str, t)) + " 1 " + '\n')
+        
+
         N = 1
-        f.write('[ distance_restraints ]\n')
+        f.write('\n[ distance_restraints ]\n')
         f.write('; staple crossovers\n')
 
         for s in top.scross:
@@ -422,27 +478,27 @@ def write_fullatom_pdb(fname, top):
             f.write(RF % (i + 1, j + 1, N) + ' '.join(map(str, CR)) + '\n')
             N += 1
 
-        f.write('; criss cross staple crossovers\n')
+        f.write('\n; criss cross staple crossovers\n')
 
         for s in top.ccross:
             i, j = s
             f.write(RF % (i + 1, j + 1, N) + ' '.join(map(str, CCR)) + '\n')
             N += 1
 
-        f.write('; scaffold crossovers\n')
+        f.write('\n; scaffold crossovers\n')
 
         for s in top.scaf.items():
             i, j = s
             f.write(RF % (i + 1, j + 1, N) + ' '.join(map(str, SR)) + '\n')
             N += 1
 
-        f.write('; lattice crossovers\n')
+        f.write('\n; lattice crossovers\n')
 
         for s in top.lattice:
             i, j = s
             f.write(RF % (i + 1, j + 1, N) + ' '.join(map(str, LR)) + '\n')
             N += 1
-
+        
 
 # In[130]:
 
